f1b7fee2df4575b8c015194f7f782bd7479a6fbb
Hamzah Al-Hagi <ha.773774142@gmail.com>
2022-06-03 10:01:16 +0300

    creat: form

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+echo " 
+<form action = 'upload_zip.php' method='post' enctype= 'multipart/form-data'>
+<input type = 'file' name ='zip_input' multiple> 
+<input tyype ='submit'>
+</form>
+";
+?>
